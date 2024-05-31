9b5cab257dae6c31008c137294fe2feae49171c6
andrex2006 <soandrea136@gmail.com>
2024-05-31 16:24:17 +0200

    added azienda db

---
+++ src/main/java/dp/esempi/security/model/Azienda.java
@@ -0,0 +1,36 @@
+package dp.esempi.security.model;
+
+import dp.esempi.security.validation.UtenteValido;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="aziende_waiting")
+public class Azienda {
+
+    @Id
+
+    @Column(name="ragione sociale")
+    private String ragione_sociale;
+    private String email;
+    private String telefono;
+    private String indirizzo;
+    @Column(name="ruolo")
+    private String role;
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "ragione sociale='" + ragione_sociale + '\'' +
+                ", email='" + email + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", indirizzo='" + indirizzo + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/dp/esempi/security/repository/AziendaRepository.java
@@ -0,0 +1,12 @@
+package dp.esempi.security.repository;
+
+import dp.esempi.security.model.Azienda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface AziendaRepository extends JpaRepository<Azienda, String> {
+
+    Optional<Azienda> findByEmail(String email);
+}
