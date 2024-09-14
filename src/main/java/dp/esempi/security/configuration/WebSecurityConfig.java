aa03a69b4be58bf3f170ebcc2612b59eebbc7956
andrex2006 <soandrea136@gmail.com>
2024-09-14 11:47:36 +0200

    fixed cors problem

--- src/main/java/dp/esempi/security/configuration/WebSecurityConfig.java
+++ src/main/java/dp/esempi/security/configuration/WebSecurityConfig.java
@@ -4,15 +4,22 @@ import dp.esempi.security.service.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -21,39 +28,28 @@ public class WebSecurityConfig {
     @Autowired
     private UtenteService utenteService;
 
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    //     return http
-    //             .csrf(AbstractHttpConfigurer::disable)
-    //             .authorizeHttpRequests(registry -> {
-    //                 registry.requestMatchers("/", "/register/**", "/login/**").permitAll();
-    //                 registry.requestMatchers("/admin/**").hasRole("ADMIN");
-    //                 registry.requestMatchers("/user/**").hasRole("USER");
-    //                 registry.anyRequest().authenticated();
-    //             })
-    //             .formLogin(formLoginConfigurer -> {
-    //                 formLoginConfigurer
-    //                         .loginPage("/login")
-    //                         .loginProcessingUrl("/login/authenticatea") 
-    //                         .successHandler(new AuthenticationSuccessHandler())
-    //                         .permitAll();
-    //             })
-    //             .build();
-    // }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://10.10.250.67","http://127.0.0.1","http://localhost","https://network.iisbadoni.it","http://network.iisbadoni.it"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("origin", "content-type", "accept", "x-requested-with"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
+                .exceptionHandling(customizer -> customizer.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(registry -> {
-                    registry.anyRequest().permitAll();
-                })
-                .formLogin(formLoginConfigurer -> {
-                    formLoginConfigurer.disable();
-                })
-                .httpBasic(basic -> basic.disable()) // Disattiva l'autenticazione di base
-                .logout(logout -> logout.disable()) // Disattiva la gestione del logout
-                .build();
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().permitAll())
+        ;
+        return http.build();
     }
 
     @Bean
